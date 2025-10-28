3c33a809b0d0ab52926548e350540b06bd689672
Tom Wennberg <towe2400@student.miun.se>
2025-10-28 20:13:15 +0100

    Defined basic Player attributes for the class

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -8,6 +8,12 @@ namespace Engine.Models
 {
     internal class Player
     {
+        string Name { get; set; } // defines a Name attribute of type string where the value can be defined "set" and read "get"
+        string CharacterClass { get; set; }
+        int HitPoints { get; set; }
+        int ExperiencePoints { get; set; } 
+        int Level { get; set; }
+        int Gold { get; set; }
 
     }
 }
