ea64a060fd948561e8e7258a9793845d77e2535b
Tom Wennberg <towe2400@student.miun.se>
2025-10-28 20:28:02 +0100

    Made class and attributes publicly accessible

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-    internal class Player
+    public class Player
     {
-        string Name { get; set; } // defines a Name attribute of type string where the value can be defined "set" and read "get"
-        string CharacterClass { get; set; }
-        int HitPoints { get; set; }
-        int ExperiencePoints { get; set; } 
-        int Level { get; set; }
-        int Gold { get; set; }
+        public string Name { get; set; } // defines a Name attribute of type string where the value can be defined "set" and read "get"
+        public string CharacterClass { get; set; }
+        public int HitPoints { get; set; }
+        public int ExperiencePoints { get; set; } 
+        public int Level { get; set; }
+        public int Gold { get; set; }
 
     }
 }
