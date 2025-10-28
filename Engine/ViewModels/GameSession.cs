deca00996b5012a8cacd1e5dbb369fbb5a6d21a6
Tom Wennberg <towe2400@student.miun.se>
2025-10-28 20:28:56 +0100

    Created a Player called CurrentPlayer from the player class. Defined the Name and Gold attributes.

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,21 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModels
+{
+    internal class GameSession
+    {
+        Player CurrentPlayer { get; set; } // Defines the Player object CurrentPlayer
+
+        public GameSession()
+        { 
+            CurrentPlayer = new Player(); // instatiates the CurrentPlayer object from the Player class
+            CurrentPlayer.Name = "Tom"; // sets the Name attribute to Tom
+            CurrentPlayer.Gold = 1000000;
+        }
+    }
+}
