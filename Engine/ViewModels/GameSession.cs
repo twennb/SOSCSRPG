b0785cd26689323188d409107e85e8ee1576e6fd
Tom Wennberg <towe2400@student.miun.se>
2025-10-30 09:27:11 +0100

    Added comments to GameSession contructor

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -9,10 +9,10 @@ namespace Engine.ViewModels
 {
     internal class GameSession
     {
-        Player CurrentPlayer { get; set; } // Defines the Player object CurrentPlayer
+        Player CurrentPlayer { get; set; } // Defines the Player property CurrentPlayer
 
-        public GameSession()
-        { 
+        public GameSession() // Done within the GameSession class just for demonstration, will be done from elsewhere
+        {                       // This constructor will set the player data when it loads, using the CurrentPlayer
             CurrentPlayer = new Player(); // instatiates the CurrentPlayer object from the Player class
             CurrentPlayer.Name = "Tom"; // sets the Name attribute to Tom
             CurrentPlayer.Gold = 1000000;
