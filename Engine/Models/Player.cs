780f2fd2b731fcebd386b7ec3ce3eb0f98f88130
Tom Wennberg <towe2400@student.miun.se>
2025-10-30 10:25:11 +0100

    Extended the changing interface to all player attributes

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -9,11 +9,54 @@ namespace Engine.Models
 {
     public class Player : INotifyPropertyChanged
     {
+        private string _name;
+        private string _characterClass;
+        private int _hitPoints;
+        private int _mana;
         private int _experiencePoints;
-        public string Name { get; set; } // defines a Name attribute of type string where the value can be defined "set" and read "get"
-        public string CharacterClass { get; set; }
-        public int HitPoints { get; set; }
-        public int Mana {  get; set; }
+        private int _level;
+        private int _gold;
+        
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        
+        } // defines a Name attribute of type string where the value can be defined "set" and read "get"
+        public string CharacterClass
+        {
+            get { return _characterClass; }
+            set
+            {
+                _characterClass = value;
+                OnPropertyChanged("CharacterClass");
+            }
+
+        }
+        public int HitPoints
+        {
+            get { return _hitPoints; }
+            set
+            {
+                _hitPoints = value;
+                OnPropertyChanged("HitPoints");
+            }
+
+        }
+        public int Mana
+        {
+            get { return _mana; }
+            set
+            {
+                _mana = value;
+                OnPropertyChanged("Mana");
+            }
+
+        }
         public int ExperiencePoints
         {
             get { return _experiencePoints; } // when something tries to get the value, show from _experiencePoints
@@ -23,8 +66,26 @@ namespace Engine.Models
                 OnPropertyChanged("ExperiencePoints"); // define which property this change updates
             }
         } 
-        public int Level { get; set; }
-        public int Gold { get; set; }
+        public int Level
+        {
+            get { return _level; }
+            set
+            {
+                _level = value;
+                OnPropertyChanged("Level");
+            }
+
+        }
+        public int Gold
+        {
+            get { return _gold; }
+            set
+            {
+                _gold = value;
+                OnPropertyChanged("Gold");
+            }
+
+        }
 
         public event PropertyChangedEventHandler PropertyChanged; // define a public event
         protected virtual void OnPropertyChanged(string propertyName)
