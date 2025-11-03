863b8143db16cda196309ab6318fe3ed2d85550e
Tom Wennberg <towe2400@student.miun.se>
2025-11-03 07:35:20 +0100

    navigation buttons now call functions to update location depending on direction moved

--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -28,5 +28,22 @@ namespace WPFUI
 
             DataContext = _gameSession; // tells the XAML file to use this data
         }
+
+        private void OnClick_MoveNorth(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveNorth();
+        }
+        private void OnClick_MoveWest(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveWest();
+        }
+        private void OnClick_MoveEast(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveEast();
+        }
+        private void OnClick_MoveSouth(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveSouth();
+        }
     }
 }
