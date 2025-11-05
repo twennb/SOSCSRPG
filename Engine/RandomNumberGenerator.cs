fbc4ced9b38cd307ceb2e01fdb347936dbd558ba
Tom Wennberg <towe2400@student.miun.se>
2025-11-05 15:42:05 +0100

    Added very simple (not so) random number generation based on a min and max value

---
+++ Engine/RandomNumberGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Engine
+{
+    public class RandomNumberGenerator
+    {
+        private static readonly Random _generator = new Random();
+
+        public static int NumberBetween(int minimumValue, int maximumValue)
+        {
+            return _generator.Next(minimumValue, maximumValue + 1);
+        }
+    }
+}
