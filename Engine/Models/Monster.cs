454019f607b8005d5f71453a337473bc211ef455
Tom Wennberg <towe2400@student.miun.se>
2025-11-05 15:21:59 +0100

    Setting up the Monster class

---
+++ Engine/Models/Monster.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Monster : BaseNotificationClass
+    {
+        private int _hitPoints;
+
+        public string Name { get; private set; }
+        public string ImageName { get; set; }
+        public int MaximumHitPoints { get; set; }
+        public int HitPoints
+        {
+            get { return _hitPoints; }
+            private set
+            {
+                _hitPoints = value;
+                OnPropertyChanged(nameof(HitPoints));
+            }
+        }
+        public int RewardExperiencePoints { get; private set; }
+        public int RewardGold { get; private set; }
+        public ObservableCollection<ItemQuantity> Inventory { get; set; }
+
+        public Monster(string name, string imageName,
+               int maxHitPoints, int hitPoints, int rewardXP, int rewardGold)
+        {
+            Name = name;
+            ImageName = imageName;
+            MaximumHitPoints = maxHitPoints;
+            HitPoints = hitPoints;
+            RewardExperiencePoints = rewardXP;
+            RewardGold = rewardGold;
+        }
+    }
+}
